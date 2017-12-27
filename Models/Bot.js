b569ec1080252404bc3f1fb75c4932eefeaf3ddc
sukitha <sukitha@duosoftware.com>
2017-12-27 16:56:21 +0530

    initial commit

---
+++ Models/Bot.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var botAppSchema = new Schema({
+    company: { type: Number, required: true },
+    tenant: { type: Number, required: true },
+    created_at: {type:Date,default: Date.now,require:true},
+    updated_at: {type:Date,default: Date.now,require:true},
+    bot_id:{type: ObjectId,ref: 'Bot'},
+    app: {type:String,require:true},
+    order:{ type: Number, required: true },
+    config: {}
+});
+
+
+var fbChannelSchema = new Schema({
+    company: { type: Number, required: true },
+    tenant: { type: Number, required: true },
+    created_at: {type:Date,default: Date.now,require:true},
+    updated_at: {type:Date,default: Date.now,require:true},
+    page_id: {type:String,require:true},
+    app_id: {type:String,require:true},
+    app_secret: {type:String,require:true},
+    page_token: {type:String,require:true},
+    verification_token: {type:String,require:true}
+
+});
+
+
+var slackChannelSchema = new Schema({
+    company: { type: Number, required: true },
+    tenant: { type: Number, required: true },
+    created_at: {type:Date,default: Date.now,require:true},
+    updated_at: {type:Date,default: Date.now,require:true},
+    client_id: {type:String,require:true},
+    client_secret: {type:String,require:true},
+    verification_token: {type:String,require:true}
+
+});
+
+
+var botSchema = new Schema({
+
+    name: {type: String, required: true, unique: true},
+    company: { type: Number, required: true },
+    tenant: { type: Number, required: true },
+    created_at: {type:Date,default: Date.now,require:true},
+    updated_at: {type:Date,default: Date.now,require:true},
+    screen_name: {type: String, required: true},
+    status:{type: Boolean, required: true},
+    channel_facebook: fbChannelSchema,
+    channel_slack: slackChannelSchema,
+    avatar: String
+});
+
+
+
+module.exports.Bot = mongoose.model('Bot', botSchema);
+module.exports.FacebookChannel = mongoose.model('FacebookChannel', fbChannelSchema);
+module.exports.SlackChannel = mongoose.model('SlackChannel', slackChannelSchema);
+botAppSchema.index({ company: 1, tenant: 1, bot_id: 1, name: 1}, { unique: true });
+module.exports.BotApp = mongoose.model('BotAppSchema', botAppSchema);
+
---
+++ MongoConnection.js
@@ -0,0 +1,90 @@
+var util = require('util');
+var mongoose = require('mongoose');
+var config = require('config');
+
+module.exports = class MongooseConnection {
+
+    constructor() {
+        this.mongoip=config.Mongo.ip;
+        this.mongoport=config.Mongo.port;
+        this.mongodb=config.Mongo.dbname;
+        this.mongouser=config.Mongo.user;
+        this.mongopass = config.Mongo.password;
+        this.mongoreplicaset= config.Mongo.replicaset;
+
+        let connectionstring = '';
+        let _mongoip = this.mongoip.split(',');
+        if(Array.isArray(_mongoip)){
+
+            if(_mongoip.length > 1){
+
+                _mongoip.forEach(function(item){
+                    connectionstring += util.format('%s:%d,',item,this.mongoport)
+                });
+
+                connectionstring = connectionstring.substring(0, connectionstring.length - 1);
+                connectionstring = util.format('mongodb://%s:%s@%s/%s',this.mongouser,this.mongopass,connectionstring,this.mongodb);
+
+                if(this.mongoreplicaset){
+                    connectionstring = util.format('%s?replicaSet=%s',connectionstring,this.mongoreplicaset) ;
+                }
+            }else{
+
+                connectionstring = util.format('mongodb://%s:%s@%s:%d/%s',this.mongouser,this.mongopass,_mongoip[0],this.mongoport,this.mongodb)
+            }
+
+        }else{
+
+            connectionstring = util.format('mongodb://%s:%s@%s:%d/%s',this.mongouser,this.mongopass,this.mongoip,this.mongoport,this.mongodb)
+        }
+
+        mongoose.connect(connectionstring,{
+            useMongoClient: true,
+            autoReconnect :true});
+
+    }
+
+}
+
+
+
+mongoose.connection.on('error', function (err) {
+    console.error( new Error(err));
+    mongoose.disconnect();
+
+});
+
+mongoose.connection.on('opening', function() {
+    console.log("reconnecting... %d", mongoose.connection.readyState);
+});
+
+
+mongoose.connection.on('disconnected', function() {
+    console.error( new Error('Could not connect to database'));
+    //mongoose.connect(connectionstring,{server:{auto_reconnect:true}});
+});
+
+mongoose.connection.once('open', function() {
+    console.log("Connected to db");
+
+});
+
+
+mongoose.connection.on('reconnected', function () {
+    console.log('MongoDB reconnected!');
+});
+
+
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
+
+
+
+
+
+
+
