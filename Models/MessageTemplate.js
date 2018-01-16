1673e52c6ed84a1a9e0d67236b02131c353e1376
Prasad Jayashanka <prasadjayashanka@gmail.com>
2018-01-16 16:34:01 +0530

    1.0.5

--- Models/MessageTemplate.js
+++ Models/MessageTemplate.js
@@ -93,6 +93,53 @@ var mediaCardSchema = new Schema({
 
 });
 
+var addressSchema = new Schema({
+    street: {type: String, required:true},
+    city: {type: String, required:true},
+    postalCode: {type: String, required:true},
+    state: {type:String},
+    province: {type:String},
+    country: {type:String, required:true}
+});
+
+var receiptAdjustmentSchema =  new Schema({
+    description: {type:String, required:true},
+    amount: {type: Double, required:true }
+
+})
+
+var receiptElement = new Schema({
+    title: {type:String, required:true},
+    subtitle: {type:String},
+    quantity: {type:Integer, default:1},
+    price: {type:Double, required:true},
+    currency: {type:String, required:true}
+    image_url: {type:String}
+});
+
+var receiptCardSchema = new Schema({
+    company: {type: Number, required: true},
+    tenant: {type: Number, required: true},
+    created_at: {type: Date, default: Date.now, require: true},
+    updated_at: {type: Date, default: Date.now, require: true},
+    type: {type: String, default:'receipt', required: true},
+    recipient_name: {type: String, required:true},
+    order_number: {type: String, required: true},
+    currency: {type: String, required: true},
+    payment_method: {type: String, required: true},
+    order_url: {type:String},
+    timestamp: {type:String},
+    shipping_address: addressSchema,
+    adjustments: [receiptAdjustmentSchema],
+    summary: {
+        sub_total: {type:Double},
+        shipping_cost: {type:Double},
+        total_tax: {type:Double},
+        total_cost: {type:Double}
+    },
+    elements: [receiptElement]
+});
+
 
 module.exports.QuickReply = mongoose.model('QuickReply', quickReplySchema);
 module.exports.ButtonList = mongoose.model('ButtonList', buttonListSchema);
