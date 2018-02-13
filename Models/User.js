55fa8108e0a58dd95c54b1dadd28686c48199362
KalanaDemel2 <kalana@duosoftware.com>
2018-02-13 10:53:56 +0530

    1.0.16

---
+++ Models/User.js
@@ -0,0 +1,13 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userMapSchema = new Schema({
+    from_id: { type: String, required: true },
+    email: { type: String, required: true },
+    bot_id: { type: String, required: true },
+    created_at: {type:Date,default: Date.now,require:true},
+    updated_at: {type:Date,default: Date.now,require:true}
+
+});
+
