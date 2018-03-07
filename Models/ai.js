14ce21b6be9b4a8b0a26e0f6e6157806c181c047
KalanaDemel2 <kalana@duosoftware.com>
2018-03-07 11:26:48 +0530

    1.0.28

--- Models/ai.js
+++ Models/ai.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var aiWorkFlowMapScheme = new Schema({
+    tenant : {type:String,require:true},
     workFlowName: {type:String,require:true},
     events:{ type : Array , "default" : [] },
     enable: Boolean
