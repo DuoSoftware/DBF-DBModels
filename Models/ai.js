1b8d8f7c6f5fac14b905ff7d717930c309b01a9e
KalanaDemel2 <kalana@duosoftware.com>
2018-03-21 15:54:14 +0530

    1.0.35

--- Models/ai.js
+++ Models/ai.js
@@ -6,13 +6,15 @@ var aiWorkFlowMapScheme = new Schema({
     tenant: {type: Number, required: true},
     created_at: {type:Date,default: Date.now,require:true},
     updated_at: {type:Date,default: Date.now,require:true},
+    ruleName: {type:String,require:true, unique: true},
+    description: {type:String},
     workFlowName: {type:String,require:true},
     events:{ type : Array , "default" : [] },
     enable: Boolean
 });
 
 
-//aiWorkFlowMapScheme.index({ "company": 1, "tenant": 1, "workFlowName": 1}, { "unique": true });
+//aiWorkFlowMapScheme.index({ "ruleName": 1, "tenant": 1, "workFlowName": 1}, { "unique": true });
 
 
 module.exports.aiWorkFlowMap = mongoose.model('aiWorkFlowMap', aiWorkFlowMapScheme);
