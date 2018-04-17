cbbe7a6ac12f0b59ae4677bf01ed0befb0b990ab
Prasad Jayashanka <prasadjayashanka@gmail.com>
2018-04-17 11:21:19 +0530

    1.0.41

--- Models/UploadFile.js
+++ Models/UploadFile.js
@@ -4,14 +4,12 @@ var Schema = mongoose.Schema;
 var uploadFileSchema = new Schema({
     company: {type: Number, required: true},
     tenant: {type: Number, required: true},
-    images: [singleFileSchema],
+    name: {type: String, default:'unspecified', required: true},
+    url: {type: String, required: true},
     created_at: {type:Date, default: Date.now, require:true},
     updated_at: {type:Date, default: Date.now, require:true}
 });
 
-var singleFileSchema = new Schema({
-    name: {type: String, default:'unspecified', required: true},
-    url: {type: String, required: true}
-});
+
 
 module.exports.UploadFile = mongoose.model('UploadFile', uploadFileSchema);
