3d14120ebaa2ef4c3b41ffd89e3225cfee815054
Pamidu jayaranga <pamidu@live.com>
2018-04-23 10:00:47 +0530

    update service fields add

--- Models/UploadFile.js
+++ Models/UploadFile.js
@@ -6,6 +6,11 @@ var uploadFileSchema = new Schema({
     tenant: {type: Number, required: true},
     name: {type: String, default:'unspecified', required: true},
     url: {type: String, required: true},
+    // date:{type:Date, default: Date.now, require:true},
+    size:{type: Number, required: true},
+    type:{type: String, required: true},
+    width:{type: Number, required: false},
+    height:{type: Number, required: false},
     created_at: {type:Date, default: Date.now, require:true},
     updated_at: {type:Date, default: Date.now, require:true}
 });
