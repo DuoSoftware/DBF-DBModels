34feef7d3e859a00571973d72977823a3fba05d4
KalanaDemel2 <kalana@duosoftware.com>
2018-05-02 10:55:17 +0530

    1.0.47

--- Models/EntityAndContext.js
+++ Models/EntityAndContext.js
@@ -22,7 +22,6 @@ var contextScheme = new Schema({
     created_at: {type:Date,default: Date.now,require:true},
     updated_at: {type:Date,default: Date.now,require:true},
     workflowName: {type:String,require:true},
-    bot_id: {type:String,require:true},
     contextMapping:[{
         entityName: {type:String,require:true},
         contextName: {type:String,require:true}
@@ -31,7 +30,7 @@ var contextScheme = new Schema({
     enable: { type : Boolean , default : true }
 });
 
-contextScheme.index({ "company": 1, "tenant": 1, "workflowName": 1, "bot_id" : 1}, { "unique": true });
+contextScheme.index({ "company": 1, "tenant": 1, "workflowName": 1}, { "unique": true });
 
 
 module.exports.entityMap = mongoose.model('entityMap', entityScheme);
