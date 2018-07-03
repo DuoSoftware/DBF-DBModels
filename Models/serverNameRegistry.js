df39fcfb8129da173c98472c70ba0f17af6dab69
KalanaDemel2 <kalana@duosoftware.com>
2018-07-03 14:24:01 +0530

    1.0.70

--- Models/serverNameRegistry.js
+++ Models/serverNameRegistry.js
@@ -5,27 +5,23 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var AutomationDnsMapScheme = new Schema({
+var serverRegistryScheme = new Schema({
     company: {type: Number, required: true},
     tenant: {type: Number, required: true},
     created_at: {type:Date,default: Date.now,require:true},
     updated_at: {type:Date,default: Date.now,require:true},
-    workflowName: {type:String,require:true},
     deploymentType : {type:String,require:true},
     serverName: {type:String},
     environment : {type:String,require:true},
     remoteUrl: {type:String,require:true},
-    proxyUrl: {type:String,require:true},
-    user: {type:String,require:true},
-    apiKey:{type:String},
     enable: { type : Boolean , default : true }
 
 
 });
 
 
-AutomationDnsMapScheme.index({ "company": 1, "tenant": 1, "workflowName": 1}, { "unique": true });
+serverRegistryScheme.index({ "company": 1, "tenant": 1, "serverName": 1}, { "unique": true });
 
-module.exports.dnsmap = mongoose.model('dnsmap', AutomationDnsMapScheme);
+module.exports.serverregistry = mongoose.model('serverregistry', serverRegistryScheme);
 
 
