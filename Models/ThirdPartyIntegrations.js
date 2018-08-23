0bcc2d48bf3d8d2bcebac6433cefaf0a32626ff3
KalanaDemel2 <kalana@duosoftware.com>
2018-08-23 09:52:24 +0530

    1.0.81

---
+++ Models/ThirdPartyIntegrations.js
@@ -0,0 +1,24 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var userIntegrationScheme = new Schema({
+    company: {type: Number, required: true},
+    tenant: {type: Number, required: true},
+    created_at: {type:Date,default: Date.now,require:true},
+    updated_at: {type:Date,default: Date.now,require:true},
+    integrationName: {type:String,require:true},
+    integrationType: {type:String,require:true},
+    integrationData:{type:Object},
+    description: {type:String},
+    enable: { type : Boolean , default : true }
+});
+
+
+
+
+userIntegrationScheme.index({ "company": 1, "tenant": 1, "integrationName": 1}, { "unique": true });
+
+
+module.exports.userIntegrationScheme = mongoose.model('userIntegrationScheme', userIntegrationScheme);
