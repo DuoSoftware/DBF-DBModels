914006f57ad1a8a4e3b21bd64935300f71ae5597
dilshanKaluarachchi <sachitra.k@duosoftware.com>
2018-09-14 15:41:26 +0530

    1.0.89

--- Models/TenantActivityRecords.js
+++ Models/TenantActivityRecords.js
@@ -9,6 +9,12 @@ var TenantActivityRecordsScheme = new Schema({
     tenant: {type: String, required: true},
     company: {type: String, required: true},
     activity_name: {type: String, required: true},
+    description: {type: String},
+    features: {type: Array},
+    tags: {type: Array},
+    what_you_get: {type: Array},
+    pricing: {type: Array},
+    faq: {type: Array},
     reviewed: {type: Boolean, required: true, default: false},
     state: {type: String},
     type: {type: String},
@@ -16,11 +22,55 @@ var TenantActivityRecordsScheme = new Schema({
     npm_module: {type: String},
     npm_version: {type: String},
     variables: {type: Array},
-    created_at: {type:Date,default: Date.now,require:true},
-    updated_at: {type:Date,default: Date.now,require:true}
+    created_at: {type:Date, default: Date.now, require:true},
+    updated_at: {type:Date, default: Date.now, require:true}
 
 });
 
-TenantActivityRecordsScheme.index({"activity_name": "1"}, {"unique": true});
+var TenantActivityVariablesScheme = new Schema({
+    tenant: {type: String, required: true},
+    company: {type: String, required: true},
+    activity_name: {type: String, required: true},
+    key: {type: String, required: true},
+    display_name: {type: String, required: true},
+    value: {type: String},
+    value_list: {type: Array},
+    api_method: {type: String},
+    type: {type: String},
+    category: {type: String},
+    data_type: {type: String},
+    group: {type: String},
+    priority: {type: String},
+    advance: {type: String},
+    control: {type: String},
+    placeholder: {type: String},
+    created_at: {type:Date, default: Date.now, require:true},
+    updated_at: {type:Date, default: Date.now, require:true}
+});
+
+var TenantActivityTagsScheme = new Schema({
+    tenant: {type: String, required: true},
+    company: {type: String, required: true},
+    activity_name: {type: String, required: true},
+    tag: {type: String, required: true},
+    created_at: {type:Date, default: Date.now, require:true},
+    updated_at: {type:Date, default: Date.now, require:true}
+});
+
+var TenantActivityPricingScheme = new Schema({
+    tenant: {type: String, required: true},
+    company: {type: String, required: true},
+    activity_name: {type: String, required: true},
+    name: {type: String},
+    pricing_fts: {type: Array},
+    price: {type: String, default: 0},
+    bill_cycle: {type: String},
+    created_at: {type:Date, default: Date.now, require:true},
+    updated_at: {type:Date, default: Date.now, require:true}
+});
+
 
 module.exports.tenantactivityrecords = mongoose.model('tenantactivityrecords', TenantActivityRecordsScheme);
+module.exports.tenantactivityvariables = mongoose.model('tenantactivityvariables', TenantActivityVariablesScheme);
+module.exports.tenantactivitytags = mongoose.model('tenantactivitytags', TenantActivityTagsScheme);
+module.exports.tenantactivitypricings = mongoose.model('tenantactivitypricings', TenantActivityPricingScheme);
