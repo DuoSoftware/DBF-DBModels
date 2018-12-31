2ff9e86d08041d7411cb6b37ed383561771df7a1
KalanaDemel2 <kalana@duosoftware.com>
2018-12-31 06:51:11 +0530

    1.0.118

--- Models/Docks.js
+++ Models/Docks.js
@@ -8,10 +8,11 @@ var DocksScheme = new Schema({
     created_at: {type:Date, default: Date.now,require:true},
     updated_at: {type:Date, default: Date.now,require:true},
     description: {type: String},
-    structure: {type: Array}
+    structure: {type: Array},
+    status:{type: Boolean, required: true, default:true}
 });
 
-DocksScheme.index({"dock_name" : 1}, {"unique": true});
+DocksScheme.index({"dock_name" : 1, "company": 1, "tenant": 1}, {"unique": true});
 
 
 module.exports.docks = mongoose.model('docks', DocksScheme);
