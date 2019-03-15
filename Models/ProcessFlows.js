587fe78672b172cc3013e42f56e108d5d711fca3
KalanaDemel2 <kalana@duosoftware.com>
2019-03-15 08:07:58 +0530

    1.0.156

--- Models/ProcessFlows.js
+++ Models/ProcessFlows.js
@@ -24,7 +24,31 @@ var WorkFlowScheme = new Schema({
 
 );
 
+var MasterFlowScheme = new Schema({
+        company: {type: Number, required: true},
+        tenant: {type: Number, required: true},
+        ID: {type: String, required: true},
+        Name: {type: String, required: true},
+        DisplayName: {type: String, required: true},
+        Description: {type: String},
+        version: {type: Array, required: true},
+        DateTime: {type: String, required: true},
+        UserName: {type: String, required: true},
+        AuthorDetails: {
+            Name: {type: String, required: true},
+            Email: {type: String, required: true},
+            Domain: {type: String, required: true}
+        },
+        Tags: {type: Array},
+
+    }
+
+
+);
+
 WorkFlowScheme.index({"ID" : 1, "company": 1, "tenant": 1}, {"unique": true});
+MasterFlowScheme.index({"ID" : 1, "company": 1, "tenant": 1}, {"unique": true});
 
 
 module.exports.workflow = mongoose.model('workflow', WorkFlowScheme);
+module.exports.masterflow = mongoose.model('masterflow', MasterFlowScheme);
