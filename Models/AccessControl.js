4f0a3dfdf295f4b555d611448f71097d4bb22585
KalanaDemel2 <kalana@duosoftware.com>
2019-03-25 11:05:51 +0530

    1.0.160

---
+++ Models/AccessControl.js
@@ -0,0 +1,16 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var accessControlSchema = new Schema({
+    permissionName : { type: String, required: true },
+    permissionObj : {},
+    created_at: {type:Date,default: Date.now},
+    updated_at: {type:Date,default: Date.now}
+});
+
+
+
+module.exports.accesscontrol = mongoose.model('accesscontrol', accessControlSchema);
+
+
