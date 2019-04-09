796260c996f543e60cdbd67eff356026f5aec37a
dilshanKaluarachchi <sachitra.k@duosoftware.com>
2019-04-09 17:23:29 +0530

    1.0.168

--- Models/ECSRecord.js
+++ Models/ECSRecord.js
@@ -10,8 +10,8 @@ var eCSRecordSchema = new Schema({
     name:{type: String, default:''},
     port:{type: Number, required: true},
     wfname:{type: String, required: true},
-    created_at: {type:Date, default: Date.now, require:true},
-    updated_at: {type:Date, default: Date.now, require:true}
+    created_at: {type:Date},
+    updated_at: {type:Date}
 });
 
 module.exports.ECSRecord = mongoose.model('ECSRecord', eCSRecordSchema);
