2321cf557745c5ddcb4fa1a0e9afa52c83e0a348
binaraGoonawardana <binara.is@live.com>
2019-04-16 15:02:01 +0530

    added new field to UserInvitation schema

--- Models/UserInvitation.js
+++ Models/UserInvitation.js
@@ -10,6 +10,7 @@ let userInvitationSchema = new Schema({
   status: { type: String, enum: ['pending','accepted','rejected','canceled'], default: 'pending' },
   company: { type: Number, required: true },
   tenant: { type: Number, required: true },
+  attributes: { type: Schema.Types.Mixed },
   created_at: { type:Date, default: Date.now, require:true },
   updated_at: { type:Date, default: Date.now, require:true}
 });
