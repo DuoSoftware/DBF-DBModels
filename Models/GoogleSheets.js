16e631f8fb372bd17d8b555638b87c74016ef17c
KalanaDemel2 <kalana@duosoftware.com>
2019-05-26 18:04:07 +0530

    commit

--- Models/GoogleSheets.js
+++ Models/GoogleSheets.js
@@ -2,14 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GoogleSheetsScheme = new Schema({
-    access_token: { type: String, required: true },
+    accessToken: { type: String, required: true },
     company: { type: String, required: true },
-    created_at: { type: Date, default: Date.now, require: true },
-    expiry_date: { type: String, required: true },
-    refresh_token: { type: String, required: true },
+    connectionID: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, require: true },
+    expiryDate: { type: String, required: true },
+    refreshToken: { type: String, required: true },
     scope: { type: String, required: true },
     tenant: { type: String, required: true },
-    token_type: { type: String, required: true },
+    tokenType: { type: String, required: true },
     updated_at: { type: Date, default: Date.now, require: true }
 });
 
@@ -25,7 +26,7 @@ var GoogleSheetsLogScheme = new Schema({
     token_type: { type: String, required: true }
 });
 
-GoogleSheetsScheme.index({ "company": 1, "tenant": 1 }, { "unique": true });
+GoogleSheetsScheme.index({ "connectionID": 1}, { "unique": true });
 GoogleSheetsLogScheme.index({ "logID": 1 }, { "unique": true });
 
 module.exports.googlesheets = mongoose.model('googlesheets', GoogleSheetsScheme);
