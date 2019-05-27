654cd24788e23321b463f0c56ea6f725aafd652a
binaraGoonawardana <binara.is@live.com>
2019-05-27 16:40:18 +0530

    added schema of Connection and module models

--- Models/AppModule.js
+++ Models/AppModule.js
@@ -1,20 +1,21 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var AppConnectionScheme = new Schema({
+var AppModuleScheme = new Schema({
   company: { type: Number, required: true },
   tenant: { type: Number, required: true },
   id: {type: String, required: true},
   name: {type: String, required: true},
+  description: {type: String, required: false},
   type: {type: String, required: true},
+  connection: {type: String, required: true},
   appId: {type: String, required: true},
   api: {type: Schema.Types.Mixed, default: {}, required: true},
-  commonData: {type: Schema.Types.Mixed, default: {}, required: true},
-  parameters: {type : Array , default : [], required: true},
+  mappableParameters: {type : Array , default : [], required: true},
   created_at: {type:Date, default: Date.now, require:true},
   updated_at: {type:Date, default: Date.now, require:true},
 }, { minimize: false }); // Mongoose will not save empty objects to database. to override use minimize
 
-AppConnectionScheme.index({id: 1, company: 1, tenant: 1}, {unique: true});
+AppModuleScheme.index({id: 1, company: 1, tenant: 1}, {unique: true});
 
-module.exports.appConnection = mongoose.model('appConnections', AppConnectionScheme);
+module.exports.appModule = mongoose.model('appModules', AppModuleScheme);
