8ce902442d091bdb69279349fdad29d9ed64cb40
dilshanKaluarachchi <sachitra.k@duosoftware.com>
2020-05-24 16:35:55 +0530

    1.0.246

---
+++ Models/CartSettings.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var CartSettingsScheme = new Schema({
+    ID: { type: String, required: true },
+    company: { type: Number, required: true },
+    tenant: { type: Number, required: true },
+    companyName: { type: String },
+    companyLogo: { type: String },
+    theme: { type: String }
+});
+
+
+CartSettingsScheme.index({ "ID": 1 }, { "unique": true });
+
+module.exports.cartSettings = mongoose.model('cartSettings', CartSettingsScheme);
